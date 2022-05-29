d0c2099edd1e98f753c7229732a0cc64105e6a75
Vova <kazap3r@gmail.com>
2022-05-29 20:49:26 +0300

    added apartmentsList

---
+++ scripts/apartmentsList.js
@@ -0,0 +1,43 @@
+let container = document.querySelector('#inject');
+let template='';
+
+
+async function  getItems(){
+    
+    let url = '../data/db.json'
+    
+    const response = await fetch(url);
+    const obj = await response.json();
+    const apartments = obj.items
+
+    apartments.forEach(apart=>{
+        template +=`
+                    <div class="a-item">
+                    <img src="${apart.modelImage}" class="a-item-image">
+                    <div class="a-item-desk">
+                        <h3>${apart.itemsTitle}</h3>
+                        <span>Квартира в ЖК Комфорт. Ідеальне інвестиційне рішення як собі, так і для здачі в оренду.</span>
+                        <a href="apartment.html?id=${apart.id}">Детальніше</a>
+                    </div>
+
+                    </div> 
+                    `
+
+
+
+      
+    })
+    container.innerHTML=template;
+    console.log(template)
+}
+
+
+
+getItems();
+
+
+/*
+
+
+
+*/
